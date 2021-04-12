6ff16b709c46e0871d38d7e02e436ef7627ee429
Jan Góral <jan.goral112@gmail.com>
2021-04-12 18:57:43 +0200

    Added ProductDataBuilder

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataBuilder.java
@@ -0,0 +1,50 @@
+package pl.com.bottega.ecommerce.sales.domain.productscatalog;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.Date;
+
+public class ProductDataBuilder {
+
+    Id productId;
+
+    Money price;
+
+    String name;
+
+    Date snapshotDate;
+
+    ProductType type;
+
+    public ProductDataBuilder() { }
+
+    public ProductDataBuilder withProductId(Id productId) {
+        this.productId = productId;
+        return this;
+    }
+
+    public ProductDataBuilder withPrice(Money price) {
+        this.price = price;
+        return this;
+    }
+
+    public ProductDataBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ProductDataBuilder withSnapshotDate(Date snapshotDate) {
+        this.snapshotDate = snapshotDate;
+        return this;
+    }
+
+    public ProductDataBuilder withProductType(ProductType type) {
+        this.type = type;
+        return this;
+    }
+
+    public ProductData build() {
+        return new ProductData(productId, price, name, type, snapshotDate);
+    }
+}
